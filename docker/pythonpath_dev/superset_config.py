bad701906a0d4622ddc15b60f7c2835d2f2db519
Mehrdad <m.azmin>
2023-08-25 19:15:34 +0330

    Disabled the HTML_SANITATION feature flag.

--- docker/pythonpath_dev/superset_config.py
+++ docker/pythonpath_dev/superset_config.py
@@ -101,7 +101,9 @@ class CeleryConfig(object):
 CELERY_CONFIG = CeleryConfig
 
 FEATURE_FLAGS = {"ALERT_REPORTS": True,
-                 "GENERIC_CHART_AXES": True}
+                 "GENERIC_CHART_AXES": True,
+                 "HTML_SANITIZATION": False}
+
 ALERT_REPORTS_NOTIFICATION_DRY_RUN = True
 WEBDRIVER_BASEURL = "http://superset:8088/"
 # The base URL for the email report hyperlinks.
